d9a0f65dc72ad1459e92de01d3eb98e39b5b88f1
Many98 <e.fratrik@gmail.com>
2023-04-04 23:06:39 +0200

    added global_vars.py module

---
+++ src/global_vars.py
@@ -0,0 +1,67 @@
+import configparser
+import json
+import os
+
+current_dir = os.path.abspath(os.curdir)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # this will ensure to always start from src directory
+
+conf = configparser.ConfigParser()
+conf.read('../config/config.ini')
+
+os.chdir(current_dir)
+################################
+# PARAMETERS USED TO           #
+# DOWNLOADING TILES            #
+# FROM ODATA-API/OPENSERCH API #
+################################
+ODATA_URI = conf['sentinel']['odata_uri']
+ODATA_RESOURCE = conf['sentinel'][
+    'odata_resource']  # FOR MORE RESOURCES SEE https://scihub.copernicus.eu/userguide/ODataAPI
+OPENSEARCH_URI = conf['sentinel']['opensearch_uri']
+ACCOUNT = conf['sentinel']['account']
+PASSWORD = conf['sentinel']['password']
+
+#########################################
+# PATHS WHERE ARE STORED SENTINEL TILES #
+#########################################
+SENTINEL_PATH_DATASET = conf['sentinel']['path_dataset']  # WHERE ARE STORED SENTINEL 2 TILES
+
+#####################################
+# PATHS WHERE ARE STORED SHAPEFILES #
+#####################################
+AGRI_PATH_DATASET = conf['agri']['path_dataset']  # SHAPEFILE OF AGRICULTURE FIELDS (ONLY)
+
+##################################
+# HYPERPARAMETERS FOR NEURAL NET #
+##################################
+INIT_LR = float(conf['network']['init_lr'])  # initial learning rate
+BATCH_SIZE = int(conf['network']['batch_size'])
+EPOCHS = int(conf['network']['epochs'])
+
+####################
+# SET RANDOM STATE #
+####################
+RANDOM_STATE = 42
+
+############
+# EPSILON  #
+############
+EPS = 1e-10
+
+
+##################################################
+# TILES USED WHEN DOWNLOADING FROM BIGGER AREA   #
+##################################################
+TILES = [i.replace(' ', '').replace('\n', '') for i in conf['tiles']['tiles'].split(',')]
+
+###############################
+# DATES USED FOR TIME SERIES  #
+###############################
+DATES = [i.replace(' ', '').replace('\n', '').replace('TO', ' TO ') for i in conf['dates']['dates'].split(',')]
+
+##############
+CLOUDS = [int(i.replace(' ', '').replace('\n', '')) for i in conf['clouds']['clouds'].split(',')]
+
+
+###############
+SEN2COR = conf['sen2cor']['sen2cor']
