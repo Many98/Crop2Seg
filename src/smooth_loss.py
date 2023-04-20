6342a35166ec31e3f86398ac47d26d4ed59fec68
Many98 <e.fratrik@gmail.com>
2023-04-20 19:44:39 +0200

    removed some comments

--- src/smooth_loss.py
+++ src/smooth_loss.py
@@ -21,20 +21,6 @@ class SmoothCrossEntropy2D(CrossEntropyLoss):
         assert input.dim() == 4, '`input` is expected to have 4 dimensions (B x N_CLASSES x H x W)'
         assert target.dim() == 3, '`target` is expected to have 3 dimensions (B x H x W)'
 
-        # cc = (np.arange(v.max()) == v[...,None]-1).astype(int).transpose(2, 0, 1)  -> one hot
-        # ot just cc = torch.nn.functional.one_hot(v).permute(2, 0, 1)
-        # dd = ndimage.binary_dilation(kk, np.ones((1, 3, 3), dtype=bool))  -> dilate
-        # dd = ndimage.binary_dilation(kk, [[False, True, False], [True, True, True], [False, True, False]])
-        #       -> or this it depends if we choose 4-connectivity or 8-connectivity
-
-        # eps=0.1/9  # 9 classes
-        # exp_small = eps * (9-dd.sum(0))
-        # exp_large = (1-exp_small)/dd.sum(0)
-
-        # target = torch.where(torch.tensor(dd)==1, torch.tensor(exp_large), torch.tensor(eps)).sum(0)
-
-        # we will need to this on the fly in loss func
-
         one_hot_target = F.one_hot(target.long(), num_classes=input.shape[1]).permute(0, 3, 1, 2)
 
         # 4-connectivity
@@ -42,7 +28,7 @@ class SmoothCrossEntropy2D(CrossEntropyLoss):
                                 [1., 1., 1.],
                                 [0., 1., 0.]]).view(1, 1, 3, 3).repeat(input.shape[1], 1, 1, 1)
 
-        # 8-connectivity
+        # TODO consider 8-connectivity
         # weights = torch.ones((input.shape[1], 1, 3, 3))
 
         # perform dilation
@@ -50,8 +36,8 @@ class SmoothCrossEntropy2D(CrossEntropyLoss):
 
         eps = self.ls / input.shape[1]
 
-        exp_small = eps * (input.shape[1] - dilated.sum(1)) #(input.shape[1] - dilated.sum(0))
-        exp_large = (1 - exp_small) / dilated.sum(1) #dilated.sum(0)
+        exp_small = eps * (input.shape[1] - dilated.sum(1))  # (input.shape[1] - dilated.sum(0))
+        exp_large = (1 - exp_small) / dilated.sum(1)  # dilated.sum(0)
 
         target = torch.where(dilated.permute(1, 0, 2, 3) == 1, exp_large, eps).permute(1, 0, 2, 3)
 
