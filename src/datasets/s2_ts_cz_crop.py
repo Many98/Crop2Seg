134b48636362ad6fec556d9c4d0f82dd17be54e0
Many98 <e.fratrik@gmail.com>
2023-05-01 20:05:09 +0200

    unpatchify func moved from helpers.utils to datasets.s2_ts_cz_crop to fix circular import

--- src/datasets/s2_ts_cz_crop.py
+++ src/datasets/s2_ts_cz_crop.py
@@ -10,6 +10,10 @@ import pandas as pd
 import torch
 import torch.utils.data as tdata
 
+import rasterio
+
+from typing import Union, Tuple
+
 # ### small boiler plate to add src to sys path
 import sys
 from pathlib import Path
@@ -19,9 +23,69 @@ root = str(file).split('src')[0]
 sys.path.append(root)
 # --------------------------------------
 
-from src.helpers.utils import unpatchify
+from src.helpers.sentinel2raster import export_to_tif
 
 logging.getLogger().setLevel(logging.INFO)
+
+
+@export_to_tif
+def unpatchify(id: int, data: np.array, metadata_path: str, nodata: int = 0, dtype: str = 'uint8',
+               export: bool = False) -> Tuple[Union[rasterio.io.DatasetReader, rasterio.io.MemoryFile], str]:
+    """
+    Function to create raster object from input `data` and using information from metadata located
+    at `metadata_path`
+    Parameters
+    ----------
+    id: int
+        Id of patch within self.metadata. It is needed to find proper affine transform and crs
+    data: np.ndarray
+        time-series of Sentinel 2 L2A raw data of shape T x C+1 x H x W
+    metadata_path: str
+        Absolute path to metadata.json file
+    nodata: int
+        Specify how to set nodata in output raster
+    dtype: str
+        Data type string representation for raster.
+        Default is uint8
+    export: bool
+        Whether to export resulting raster. Raster will be exported to directory `export` near to `metadata_path`
+    """
+    assert 'metadata.json' in metadata_path, '`metadata_path` is expected to have filename `metadata.json`'
+    assert os.path.isfile(metadata_path), f'`metadata_path` ({metadata_path}) is now valid path'
+
+    metadata = pd.read_json(metadata_path, orient='records', dtype={'ID_PATCH': 'int32',
+                                                                    'ID_WITHIN_TILE': 'int32',
+                                                                    'Background_Cover': 'float32',
+                                                                    'time-series_length': 'int16',
+                                                                    'crs': 'int16',
+                                                                    'Fold': 'int8'})
+
+    assert data.ndim == 2, '`data` array is expected to be 2d (matrix)'
+
+    affine = metadata[metadata['ID_PATCH'] == id]['affine'].values[0]
+    crs_ = metadata[metadata['ID_PATCH'] == id]['crs'].values[0]
+
+    profile = {'driver': 'GTiff', 'dtype': dtype, 'nodata': nodata, 'width': data.shape[1],
+               'height': data.shape[0], 'count': 1,
+               'crs': rasterio.crs.CRS.from_epsg(crs_),
+               'transform': rasterio.Affine(affine[0][0], affine[1][0], affine[2][0],
+                                            affine[0][1], affine[1][1], affine[2][1]),
+               'blockxsize': 128,
+               'blockysize': 128, 'tiled': True, 'compress': 'lzw'}
+
+    # TODO MemoryFile does not empties /vsim/... -> stop using it ... use just plain np.array accessed via read method
+    #  use NamedTemporaryFile instead https://rasterio.readthedocs.io/en/stable/topics/memory-files.html
+    memfile = rasterio.io.MemoryFile(filename=f'raster_{id}.tif')
+    with memfile.open(**profile) as rdata:
+        rdata.write(data[None, ...].astype('uint8'))  # write the data
+
+    # with rasterio.open('example.tif', 'w', **profile) as dst:
+    #         dst.write(data, 1)
+
+    if export:
+        os.makedirs(os.path.join(os.path.split(metadata_path)[0], 'export'), exist_ok=True)
+
+    return memfile.open(), os.path.join(os.path.split(metadata_path)[0], 'export', f'raster_{id}.tif')
 
 
 class S2TSCZCropDataset(tdata.Dataset):
@@ -35,6 +99,7 @@ class S2TSCZCropDataset(tdata.Dataset):
         - target is the semantic or instance target
     TODO adjust it to handle also pretrain dataset ... difference should be only in loading TARGET
     """
+
     def __init__(
             self,
             folder,
