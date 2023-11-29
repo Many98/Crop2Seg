020f77bb83a60551384771fb8691b6ac83b76732
Many98 <e.fratrik@gmail.com>
2023-11-29 22:25:51 +0100

    added recunet implementation

---
+++ src/backbones/recunet.py
@@ -0,0 +1,171 @@
+import torch
+import torch.nn as nn
+from einops import repeat
+
+from src.backbones.temporal_aggregator import TemporalAggregator
+from src.backbones.conv import ConvBlock, UpConvBlock, DownConvBlock
+from src.backbones.convlstm import ConvLSTM, BConvLSTM
+
+
+class RecUNet(nn.Module):
+    """Recurrent U-Net architecture. Similar to the U-TAE architecture but
+    the L-TAE is replaced by a recurrent network
+    and temporal averages are computed for the skip connections."""
+
+    def __init__(
+        self,
+        input_dim,
+        encoder_widths=[64, 64, 64, 128],
+        decoder_widths=[32, 32, 64, 128],
+        out_conv=[32, 20],
+        str_conv_k=4,
+        str_conv_s=2,
+        str_conv_p=1,
+        temporal="lstm",
+        input_size=128,
+        encoder_norm="group",
+        hidden_dim=128,
+        encoder=False,
+        padding_mode="reflect",
+        pad_value=0,
+    ):
+        super(RecUNet, self).__init__()
+        self.n_stages = len(encoder_widths)
+        self.temporal = temporal
+        self.encoder_widths = encoder_widths
+        self.decoder_widths = decoder_widths
+        self.enc_dim = (
+            decoder_widths[0] if decoder_widths is not None else encoder_widths[0]
+        )
+        self.stack_dim = (
+            sum(decoder_widths) if decoder_widths is not None else sum(encoder_widths)
+        )
+        self.pad_value = pad_value
+
+        self.encoder = encoder
+        if encoder:
+            self.return_maps = True
+        else:
+            self.return_maps = False
+
+        if decoder_widths is not None:
+            assert len(encoder_widths) == len(decoder_widths)
+            assert encoder_widths[-1] == decoder_widths[-1]
+        else:
+            decoder_widths = encoder_widths
+
+        self.in_conv = ConvBlock(
+            nkernels=[input_dim] + [encoder_widths[0], encoder_widths[0]],
+            pad_value=pad_value,
+            norm=encoder_norm,
+        )
+
+        self.down_blocks = nn.ModuleList(
+            DownConvBlock(
+                d_in=encoder_widths[i],
+                d_out=encoder_widths[i + 1],
+                k=str_conv_k,
+                s=str_conv_s,
+                p=str_conv_p,
+                pad_value=pad_value,
+                norm=encoder_norm,
+                padding_mode=padding_mode,
+            )
+            for i in range(self.n_stages - 1)
+        )
+        self.up_blocks = nn.ModuleList(
+            UpConvBlock(
+                d_in=decoder_widths[i],
+                d_out=decoder_widths[i - 1],
+                d_skip=encoder_widths[i - 1],
+                k=str_conv_k,
+                s=str_conv_s,
+                p=str_conv_p,
+                norm=encoder_norm,
+                padding_mode=padding_mode,
+            )
+            for i in range(self.n_stages - 1, 0, -1)
+        )
+        self.temporal_aggregator = TemporalAggregator(mode="mean")
+
+        if temporal == "mean":
+            self.temporal_encoder = TemporalAggregator(mode="mean")
+        elif temporal == "lstm":
+            size = int(input_size / str_conv_s ** (self.n_stages - 1))
+            self.temporal_encoder = ConvLSTM(
+                input_dim=encoder_widths[-1],
+                input_size=(size, size),
+                hidden_dim=hidden_dim,
+                kernel_size=(3, 3),
+            )
+            self.out_convlstm = nn.Conv2d(
+                in_channels=hidden_dim,
+                out_channels=encoder_widths[-1],
+                kernel_size=3,
+                padding=1,
+            )
+        elif temporal == "blstm":
+            size = int(input_size / str_conv_s ** (self.n_stages - 1))
+            self.temporal_encoder = BConvLSTM(
+                input_dim=encoder_widths[-1],
+                input_size=(size, size),
+                hidden_dim=hidden_dim,
+                kernel_size=(3, 3),
+            )
+            self.out_convlstm = nn.Conv2d(
+                in_channels=2 * hidden_dim,
+                out_channels=encoder_widths[-1],
+                kernel_size=3,
+                padding=1,
+            )
+        elif temporal == "mono":
+            self.temporal_encoder = None
+        self.out_conv = ConvBlock(nkernels=[decoder_widths[0]] + out_conv, padding_mode=padding_mode)
+
+    def forward(self, input, batch_positions=None):
+        pad_mask = (
+            (input == self.pad_value).all(dim=-1).all(dim=-1).all(dim=-1)
+        )  # BxT pad mask
+
+        out = self.in_conv.smart_forward(input)
+
+        feature_maps = [out]
+        # ENCODER
+        for i in range(self.n_stages - 1):
+            out = self.down_blocks[i].smart_forward(feature_maps[-1])
+            feature_maps.append(out)
+
+        # Temporal encoder
+        if self.temporal == "mean":
+            out = self.temporal_encoder(feature_maps[-1], pad_mask=pad_mask)
+        elif self.temporal == "lstm":
+            _, out = self.temporal_encoder(feature_maps[-1], pad_mask=pad_mask)
+            out = out[0][1]  # take last cell state as embedding
+            out = self.out_convlstm(out)
+        elif self.temporal == "blstm":
+            out = self.temporal_encoder(feature_maps[-1], pad_mask=pad_mask)
+            out = self.out_convlstm(out)
+        elif self.temporal == "mono":
+            out = feature_maps[-1]
+
+        if self.return_maps:
+            maps = [out]
+        for i in range(self.n_stages - 1):
+            if self.temporal != "mono":
+                skip = self.temporal_aggregator(
+                    feature_maps[-(i + 2)], pad_mask=pad_mask
+                )
+            else:
+                skip = feature_maps[-(i + 2)]
+            out = self.up_blocks[i](out, skip)
+            if self.return_maps:
+                maps.append(out)
+
+        if self.encoder:
+            return out, maps
+        else:
+            out = self.out_conv(out)
+            if self.return_maps:
+                return out, maps
+            else:
+                return out
